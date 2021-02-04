0ffbb62b2819850147eb5711c7ff71d8f1ba8291
TheRealFrostyFace <60528755+TheRealFrostyFace@users.noreply.github.com>
2021-02-04 17:49:30 -0500

    Input KeyCode Update
    Completed the Input Manager with the rest of the KeyCodes and added a few new key abilities.

--- Poof/Assets/Scripts/Input/InputManager.cs
+++ Poof/Assets/Scripts/Input/InputManager.cs
@@ -4,6 +4,30 @@ namespace myInput
 {
     public static class InputManager
     {
+
+#region Left
+        public static bool moveLeft()
+        {
+            return Input.GetKey(KeyBinds.moveLeft);
+        }
+
+#endregion
+
+#region Right
+        public static bool moveRight()
+        {
+            return Input.GetKey(KeyBinds.moveRight);
+        }
+
+        #endregion
+
+#region Dash
+        public static bool getdash()
+        {
+            return Input.GetKey(KeyBinds.dash);
+        }
+#endregion
+
 #region Jump
         public static bool getJump()
         {
@@ -19,10 +43,47 @@ namespace myInput
         {
             return Input.GetKeyUp(KeyBinds.jump) || Input.GetKeyUp(KeyBinds.altJump);
         }
+        #endregion
+
+#region Crouch
+        public static bool crouch()
+        {
+            return Input.GetKey(KeyBinds.crouch);
+        }
+
+#endregion
+
+#region SyncRecord
+        public static bool startSync()
+        {
+            return Input.GetKey(KeyBinds.startSync);
+        }
+        public static bool recordSync()
+        {
+            return Input.GetKey(KeyBinds.recordSync);
+        }
+        public static bool toggleSync()
+        {
+            return Input.GetKey(KeyBinds.toggleSync);
+        }
+
+#endregion
+
+#region Attack
+        public static bool attack()
+        {
+            return Input.GetKey(KeyBinds.attack);
+        }
+
+        #endregion
+
+#region Interact
+        public static bool interact()
+        {
+            return Input.GetKey(KeyBinds.interact);
+        }
 #endregion
 
 
-
-        //crouch stuff
     }
 }
--- Poof/Assets/Scripts/Input/KeyBinds.cs
+++ Poof/Assets/Scripts/Input/KeyBinds.cs
@@ -6,14 +6,17 @@ namespace myInput
     {
         public static KeyCode moveLeft = KeyCode.A;
         public static KeyCode moveRight = KeyCode.D;
+        public static KeyCode dash = KeyCode.LeftShift;
         public static KeyCode jump = KeyCode.W;
-        public static KeyCode crouch = KeyCode.S;
         public static KeyCode altJump = KeyCode.Space;
+        public static KeyCode crouch = KeyCode.S;
 
         public static KeyCode startSync = KeyCode.Q;
         public static KeyCode recordSync = KeyCode.E;
         public static KeyCode toggleSync = KeyCode.R;
 
-        public static KeyCode attack = KeyCode.F;
+        public static KeyCode attack = KeyCode.Mouse0;
+        public static KeyCode interact = KeyCode.F;
+
     }
 }
