bfebf847ae8786eff97d249b885184fe6ac50897
Kris <32527961+creepyfridge@users.noreply.github.com>
2021-02-09 20:58:53 -0500

    started a character controller mevement script
    deleted an empty stub with no name
    removed the 1 frame input smoothing

---
+++ Poof/Assets/Scripts/Player/CCMovement2D.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myInput;
+
+[RequireComponent(typeof(CharacterController))]
+public class CCMovement2D : MonoBehaviour
+{
+    CharacterController _controller = null;
+    float _speed = 0.0f;
+
+    
+    [SerializeField]
+    float maxSpeed = 20.0f;
+    [SerializeField]
+    [Range(0.0f,1.0f)]
+    float instantSpeed = 0.7f;
+    [SerializeField]
+    float accelTime = 1.5f;
+
+    [SerializeField]
+    float passiveStopSpeed = 0.35f;
+
+    [SerializeField]
+    float gravity = 9.8f;
+    [SerializeField]
+    float terminalVelocity = 30.0f;
+
+    float lastInputX = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.GetComponent<CharacterController>();
+        if (_controller == null)
+            Debug.LogError("missing character controller on player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded())
+            groundedMovement();
+        else
+            airborneMovement();
+
+        _controller.Move(new Vector3(0, 0, _speed) * Time.deltaTime);
+    }
+
+    //needed because the character crontroller grounded check can fail
+    bool isGrounded()
+    {
+        return true;
+    }
+
+    void groundedMovement()
+    {
+        //-1.0 <-> 1.0
+        //left <-> right
+        float input = InMan.getHorizontal();
+
+        if (lastInputX != input && input != 0.0f)
+        {
+            //direction change/started moving
+            _speed = maxSpeed * instantSpeed * input;
+        }
+        else if(input != 0)
+        {
+            //player is holding button
+            _speed += maxSpeed/(accelTime) * Time.deltaTime * input;
+            _speed = Mathf.Clamp(_speed, -maxSpeed, maxSpeed);
+        }
+        else if(_speed != 0.0f)
+        {
+            //not moving/stopping
+            float slow = passiveStopSpeed * Time.deltaTime;
+            if (_speed < 0.0f)
+                slow *= -1.0f;
+            _speed -= slow;
+
+            if (Mathf.Abs(_speed) < 0.05f)
+                _speed = 0.0f;
+        }
+        lastInputX = input;
+    }
+
+    void airborneMovement()
+    {
+
+    }
+
+    void applyGravity()
+    {
+
+    }
+
+    void jump()
+    {
+
+    }
+}
